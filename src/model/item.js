dcd41a3579c409350467000221a9ea8226336428
Neylix <julien.leclerc05@protonmail.com>
2021-03-05 22:59:20 +0100

    delete model folder

--- src/model/item.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const itemSchema = new mongoose.Schema({
-  companyId: {type: mongoose.ObjectId, required: true},
-  name: { type: String, required: true },
-  price: { type: Number, required: true }
-});
-
-export default mongoose.model('Item', itemSchema);
